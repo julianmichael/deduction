e630de6eeebc45e340b1d4fe553d7ef9d49b0ce4
julianmichael <julianjohnmichael@gmail.com>
2014-06-18 23:57:03 -0700

    Refactored parsing into a submodule and incorporated into build

---
+++ project/Build.scala
@@ -0,0 +1,8 @@
+import sbt._
+import Keys._
+
+object MyBuild extends Build {
+  lazy val deduction = Project("deduction", file(".")).
+    dependsOn(parsing % "compile;test->test")
+  lazy val parsing = RootProject( file("lib/parsing") )
+}
